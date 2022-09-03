932716075f6e26d556110f3168cfc584701c23c9
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2022-09-03 22:46:52 +0100

    🎨 IMPROVE: Add structure for model

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -3,13 +3,15 @@
 namespace App\Controller\Pages;
 
 use App\Controller\Utils\View;
+use App\Model\Entity\User;
 
 class Home extends Page
 {
     public static function getHome()
     {
+        $user = new User();
         $content = View::render('pages/home', [
-            'title' => 'Home',
+            'title' => $user->name,
             'content' => 'This is the home page.'
         ]);
 
---
+++ app/Model/Entity/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Entity;
+
+class User {
+    public $id = 1;
+    public $name = 'John';
+    public $email = 'john@gmail.com';
+    public $password = '123456';
+}
